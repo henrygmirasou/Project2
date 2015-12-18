8c119455aaed5168d737709eccba7ff0453a8f13
student <none@none.com>
2015-12-17 21:32:47 -0800

    Last work I could do before I had to leave for home. I gave it my all professor.

--- dal/db_connection.js
+++ dal/db_connection.js
@@ -1,6 +1,6 @@
 exports.config = {
-    user: 'mirassou',
-    password: '4259026',
-    host: 'cwolf.cs.sonoma.edu',
-    database: 'mirassou'
+    user: 'mou',
+    password: '',
+    host: 'cwolf.cs.edu',
+    database: 'mi'
 };
